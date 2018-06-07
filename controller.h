28afad8de34ca6391eb2074b4d08b409ffc1a7b3
urgrod <lecam.gurvan@gmail.com>
2018-06-07 16:54:26 +0200

    slot

---
+++ controller.h
@@ -0,0 +1,47 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <QString>
+
+class controller
+{
+public:
+    controller();
+
+public slots:
+    void slotLogin;
+
+    //slot proposition
+    void slotCreateProposition;
+    void slotReadAllProposition;
+    void slotReadOneProposition;
+    void slotUpdateProposition;
+    void slotDeleteProposition;
+
+    //slot question
+    void slotCreateQuestion;
+    void slotReadAllQuestion;
+    void slotReadOneQuestion;
+    void slotUpdateQuestion;
+    void slotDeleteQuestion;
+
+    //slot theme
+    void slotCreateTheme;
+    void slotReadAllTheme;
+    void slotReadOneTheme;
+    void slotUpdateTheme;
+    void slotDeleteTheme;
+
+    //slot user
+    void slotCreateUser;
+    void slotReadAllUser;
+    void slotReadOneUser;
+    void slotUpdateUser;
+    void slotDeleteUser;
+
+private:
+    QString pseudo;
+    bool auth;
+};
+
+#endif // CONTROLLER_H
